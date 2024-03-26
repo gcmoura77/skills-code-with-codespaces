dc1bf92af0817ec3cef537b2a28841cefa3fd7b3
gcmoura77 <108241168+gcmoura77@users.noreply.github.com>
2024-03-26 14:00:12 +0000

    Criação de projeto para testes com C#

---
+++ SololearnStudy/Program.cs
@@ -0,0 +1,52 @@
+﻿abstract class Figure
+{
+    //define abstract method Perimeter with no body
+    public abstract int Perimeter();
+}
+
+class Rectangle : Figure
+{
+    public int width;
+    public int height;
+
+    public Rectangle(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    //override Perimeter method for rectangle
+    public override int Perimeter(){
+        return ((2*this.width)+(2*this.height));
+    }
+    
+}
+class Triangle : Figure
+{
+    public int side1;
+    public int side2;
+    public int side3;
+
+    public Triangle(int s1, int s2, int s3)
+    {
+        this.side1 = s1;
+        this.side2 = s2;
+        this.side3 = s3;
+    }
+    
+    //override Perimeter method for rectangle
+    public override int Perimeter() {
+        return (this.side1+this.side2+this.side3);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Figure rectangle = new Rectangle(5, 6);
+        Figure triangle = new Triangle(4, 8, 3);
+
+        Console.WriteLine(rectangle.Perimeter());
+        Console.WriteLine(triangle.Perimeter());
+    }
+}
